3ffbad804357a90afceb84b1fa942cd007374f8a
Joseph Ndemo <joseph.ndemo@student.moringaschool.com>
2025-12-10 13:36:02 -0500

    change variables to const

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 //Lab: Flatburger Restaurant
 
 //declare variable
-let burgers = ['Hamburger', 'Cheeseburger'];  
+const burgers = ['Hamburger', 'Cheeseburger'];  
 let featuredDrink = 'Strawberry Milkshake';   
 
 // Function and Block
 function addBurger() {
-    let newBurger = 'Flatburger';  
+    const newBurger = 'Flatburger';  
     burgers.push(newBurger);      
 }
 
 //Variable Declaration and Scope
 if (true) {   
-    let anotherNewBurger = 'Maple Bacon Burger';  
+    const anotherNewBurger = 'Maple Bacon Burger';  
     burgers.push(anotherNewBurger);
 
     function changeFeaturedDrink() {
